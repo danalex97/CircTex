8b1b66df1ae6a9eb6cb4ceaea1a8c4ad6d9092b5
Alexandru Dan <ad5915@voxel04.doc.ic.ac.uk>
2015-11-06 08:48:39 +0000

    Implemented MinTerm detection.

--- DNF.hs
+++ DNF.hs
@@ -10,11 +10,45 @@ type MinTerm = [Exp]
 -- a context is composed of all the variables in a boolean expression
 
 getContext :: Exp -> Context
-getContext = nub . getContext'
+getContext 
+  = nub . getContext'
   where   
     getContext' ( BinApp op a b ) = getContext a ++ getContext b
     getContext' ( Not a )         = getContext a
     getContext' ( Val a )         = []
     getContext' ( Id  a )         = [a] 
 
+binaries :: [[State]]
+binaries = [] : [ new : old | old <- binaries, new <- [F,T] ]
 
+confs :: Int -> [[State]]
+confs n 
+  = dropWhile (\x -> length x < n) $ takeWhile (\x -> length x <= n) binaries
+
+getEnvs :: Exp -> [Env]
+getEnvs exp 
+  = zipWith zip ( repeat context ) ( confs l )
+  where 
+    context = getContext exp
+    l       = length context
+
+buildMinTerm :: Env -> MinTerm
+buildMinTerm = map (\(s,v) -> if v == T then (Id s) else (!) (Id s) )
+
+getMinterms :: Exp -> [MinTerm]
+getMinterms exp 
+  = map buildMinTerm trueEnvs 
+  where 
+    trueEnvs = filter (\x -> evalExp exp x == T) envs
+    envs     = getEnvs exp
+
+simpleDNF :: Exp -> Exp
+-- gets a left associative DNF
+simpleDNF exp
+  = foldl1 (+) $ map ( foldl1 (*) ) minTerms
+  where 
+    minTerms = getMinterms exp
+
+eachTwoWith :: Exp -> ( Exp -> Exp -> Exp ) -> Exp
+-- groups a set of MinTerms at half with respect to an operation
+eachTwoWith = undefined
